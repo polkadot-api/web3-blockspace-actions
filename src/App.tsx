ca3a024eba27cc4ff1d0882980772ae458d6bc29
Victor Oliva <olivarra1@gmail.com>
2024-08-19 15:31:25 +0200

    add account picker

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Suspense } from "react"
 import { Route, Routes } from "react-router-dom"
 import { Void } from "./actions"
+import { Header } from "./Header"
 
 function App() {
   return (
     <div className="shadow max-w-screen-md m-auto p-2">
+      <Header />
       <Suspense fallback="Loading">
         <Routes>
           <Route path="/" element={<Void />} />
