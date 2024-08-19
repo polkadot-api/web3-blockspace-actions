3c4db6bfad83f130cb2a11a6d9c9358ebc61c142
Hannah Redler <hannahjredler@gmail.com>
2024-08-19 18:46:45 +0200

    Add sender details section, move the accounts selector into its own component

--- src/actions/send/SendAction.tsx
+++ src/actions/send/SendAction.tsx
@@ -8,6 +8,8 @@ import {
 } from "./send"
 import { formatCurrency } from "../../utils/format-currency.ts"
 import Void from "../void.tsx"
+import { selectedAccount$ } from "@/services/accounts.ts"
+import { AccountSelector } from "@/AccountSelector.tsx"
 
 recipientChainData$.subscribe()
 transferAmount$.subscribe()
@@ -25,8 +27,8 @@ export const SendAction = () => {
   return (
     <div className="flex flex-col text-center items-center ">
       <h1 className="text-lg my-5 font-semibold">Send Tokens</h1>
-      <div className="flex flex-col text-left  border-[1px] border-gray-200 rounded-lg p-5">
-        <h2 className="text-lg font-semibold">Transfer Information</h2>
+      <div className="flex flex-col min-w-[350px] text-left  border-[1px] border-gray-200 rounded-lg p-5 mb-5">
+        <h2 className="text-lg font-semibold">Recipient Details</h2>
         <div className="flex flex-row justify-between">
           Amount:{" "}
           <div className="text-right">
@@ -42,11 +44,18 @@ export const SendAction = () => {
           <div className="text-right">{truncateAddress(recipient ?? "")}</div>
         </div>
       </div>
+      <div className="flex flex-col min-w-[350px] text-left  border-[1px] border-gray-200 rounded-lg p-5 mb-5">
+        <h2 className="text-lg font-semibold">Sender Details</h2>
+        <div className="flex flex-row justify-between">
+          Sender account:
+          <AccountSelector />
+        </div>
+      </div>
     </div>
   )
 }
 
 export const truncateAddress = (address: SS58String) => {
   if (address.length < 6) return address
-  return address.slice(0, 3) + "..." + address.slice(address.length - 3)
+  return address.slice(0, 4) + "..." + address.slice(address.length - 4)
 }
